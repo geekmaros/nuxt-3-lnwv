1287671d9d638279296120337e8b8f5274cb3300
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2023-06-03 21:02:15 +0100

    feature: added error handler for client and server side

---
+++ error.vue
@@ -0,0 +1,38 @@
+<script setup>
+const error = useError();
+const handleError = () => {
+    clearError({
+        redirect:
+            "/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
+    })
+};
+</script>
+<template>
+<NuxtLayout>
+  <div class="flex flex-col">
+      <div v-if="error.statusCode === '404'" class="prose">
+          <h1>404</h1>
+          <p>Page not found</p>
+      </div>
+      <div  v-else class="prose">
+          <h1>Dang</h1>
+          <p>Something went wrong</p>
+          <p>Sorry about that</p>
+
+      </div>
+      <div class="prose mt-5">
+          <p>
+              <strong>
+                  {{ error.message }}
+              </strong>
+          </p>
+          <p>
+              Go to the
+              <a @click="handleError" class="hover:cursor-pointer">
+                  first lesson
+              </a>
+          </p>
+      </div>
+  </div>
+</NuxtLayout>
+</template>
